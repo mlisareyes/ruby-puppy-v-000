ab5295862bebfe4df61093e9b733e38ba8af61cd
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-07 06:42:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,13 +8,11 @@ class Dog
     @@all << self
   end
 
-  def clear_all.all
-    @@all.empty
-  
-  end
-
   def self.all
-    @@all
+    @@all.each{|dog| puts dog.name}
   end
 
+  def self.clear_all
+    @@all.clear
+  end
 end
